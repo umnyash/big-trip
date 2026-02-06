7b321303ce83ae268c9e50d3a400dd4cc09fb0c6
Rustam Sadreev <rustam17.sb@gmail.com>
2026-02-06 23:58:48 +0300

    feat(presenter): add TripPresenter

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import TripPresenter from './presenter/trip-presenter.js';
+
+const tripHeaderElement = document.querySelector('.trip__header');
+
+const tripPresenter = new TripPresenter({
+  headerElement: tripHeaderElement,
+});
+
+tripPresenter.init();
