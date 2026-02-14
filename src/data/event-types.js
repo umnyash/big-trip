a13c6f2227663a94799d32de0370f55ed5094b4b
Rustam Sadreev <rustam17.sb@gmail.com>
2026-02-14 18:07:21 +0300

    feat(data): add event types

---
+++ src/data/event-types.js
@@ -0,0 +1,42 @@
+const eventTypes = {
+  'taxi': {
+    title: 'Taxi',
+    iconUrl: 'img/icons/event-types/taxi.png',
+  },
+  'bus': {
+    title: 'Bus',
+    iconUrl: 'img/icons/event-types/bus.png',
+  },
+  'train': {
+    title: 'Train',
+    iconUrl: 'img/icons/event-types/train.png',
+  },
+  'ship': {
+    title: 'Ship',
+    iconUrl: 'img/icons/event-types/ship.png',
+  },
+  'drive': {
+    title: 'Drive',
+    iconUrl: 'img/icons/event-types/drive.png',
+  },
+  'flight': {
+    title: 'Flight',
+    iconUrl: 'img/icons/event-types/flight.png',
+  },
+  'check-in': {
+    title: 'Check-in',
+    iconUrl: 'img/icons/event-types/check-in.png',
+  },
+  'sightseeing': {
+    title: 'Sightseeing',
+    iconUrl: 'img/icons/event-types/sightseeing.png',
+  },
+  'restaurant': {
+    title: 'Restaurant',
+    iconUrl: 'img/icons/event-types/restaurant.png',
+  },
+};
+
+const eventTypeIds = Object.keys(eventTypes);
+
+export { eventTypes, eventTypeIds };
