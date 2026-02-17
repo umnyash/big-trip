185eaeeea0b1354cfe6b33a646104c0a567f6d63
Rustam Sadreev <rustam17.sb@gmail.com>
2026-02-17 18:07:43 +0300

    feat(presenter): use TripModel in TripPresenter

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import TripModel from './model/trip-model.js';
 import TripPresenter from './presenter/trip-presenter.js';
 
 const tripHeaderElement = document.querySelector('.trip__header');
+const tripModel = new TripModel();
 
 const tripPresenter = new TripPresenter({
   headerElement: tripHeaderElement,
+  model: tripModel,
 });
 
 tripPresenter.init();
