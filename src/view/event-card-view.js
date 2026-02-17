f48694771ba57dfcff2cc48eac288782cb9b87cd
Rustam Sadreev <rustam17.sb@gmail.com>
2026-02-17 21:18:39 +0300

    feat(view): render event card using provided data

--- src/view/event-card-view.js
+++ src/view/event-card-view.js
@@ -1,28 +1,50 @@
 import { createElement } from '../framework';
+import { eventTypes } from '../data';
+import { formatTime, formatDay, getFormattedDuration } from '../utils';
 
-function createEventCardTemplate() {
+function createEventCardOffersTemplate(event, offers) {
+  if (!event.offerIds.length) {
+    return '';
+  }
+
+  return (
+    `<h4 class="visually-hidden">Offers</h4>
+    <ul class="event-card__offers">
+      ${event.offerIds.map((id) => `
+        <li class="event-card__offer">
+          ${offers[id].title} +&#8288;€&nbsp;${offers[id].price}
+        </li>
+      `).join('')}
+    </ul>`
+  );
+}
+
+function createEventCardTemplate({ destination, event, offers }) {
+  const { startDate, endDate, basePrice, isFavorite } = event;
+
+  const type = eventTypes[event.type];
+  const day = formatDay(startDate);
+  const startTime = formatTime(startDate);
+  const endTime = formatTime(endDate);
+  const duration = getFormattedDuration(startDate, endDate);
+
   return (
     `<li class="event-list__item">
       <article class="event-list__card event-card">
-        <h3 class="event-card__title">Bus Berlin</h3>
+        <h3 class="event-card__title">${type.title} ${destination.name}</h3>
         <span class="event-card__icon event-icon" aria-hidden="true">
-          <img class="event-icon__image" src="img/icons/event-types/bus.png" width="18" height="18" alt="">
+          <img class="event-icon__image" src="${type.iconUrl}" width="18" height="18" alt="">
         </span>
-        <time class="event-card__day" datetime="2026-08-14">Aug 14</time>
+        <time class="event-card__day" datetime="${startDate}">${day}</time>
         <p class="event-card__time">
-          <time datetime="2026-08-14T09:00">09:00</time>
+          <time datetime="${startDate}">${startTime}</time>
           &mdash;
-          <time datetime="2026-08-14T09:45">09:45</time>
+          <time datetime="${endDate}">${endTime}</time>
         </p>
-        <p class="event-card__duration">45m</p>
-        <p class="event-card__price">€&nbsp;25</p>
-        <h4 class="visually-hidden">Offers</h4>
-        <ul class="event-card__offers">
-          <li class="event-card__offer">Choose seats +&#8288;€&nbsp;5</li>
-          <li class="event-card__offer">Infotainment system +&#8288;€&nbsp;3</li>
-          <li class="event-card__offer">Order meal +&#8288;€&nbsp;7</li>
-        </ul>
-        <button class="event-card__favorite-button favorite-button" type="button" aria-pressed="true">
+        <p class="event-card__duration">${duration}</p>
+        <p class="event-card__price">€&nbsp;${basePrice}</p>
+        ${createEventCardOffersTemplate(event, offers)}
+        <button class="event-card__favorite-button favorite-button" type="button" aria-pressed="${isFavorite}">
           <span class="visually-hidden">Add to favorite</span>
         </button>
         <button class="event-card__edit-button arrow-button" type="button">
@@ -34,8 +56,18 @@ function createEventCardTemplate() {
 }
 
 export default class EventCardView {
+  constructor({ event, destination, offers }) {
+    this.event = event;
+    this.destination = destination;
+    this.offers = offers;
+  }
+
   getTemplate() {
-    return createEventCardTemplate();
+    return createEventCardTemplate({
+      event: this.event,
+      destination: this.destination,
+      offers: this.offers,
+    });
   }
 
   getElement() {
