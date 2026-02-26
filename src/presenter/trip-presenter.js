dfd45adde39b7082650acca9d4932b19c7198fd1
Rustam Sadreev <rustam17.sb@gmail.com>
2026-02-26 03:35:03 +0300

    feat(presenter): render message when no events

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -9,6 +9,7 @@ import TripFilterView from '../view/trip-filter-view.js';
 import TripPriceView from '../view/trip-price-view.js';
 import TripSortView from '../view/trip-sort-view.js';
 import TripTitleView from '../view/trip-title-view.js';
+import TripMessage, { MessageVariant } from '../view/trip-message-view.js';
 
 export default class TripPresenter {
   #headerElement = null;
@@ -33,11 +34,22 @@ export default class TripPresenter {
   }
 
   #render() {
+    render(new TripFilterView(), this.#headerElement);
+    render(new AddEventButtonView(), this.#headerElement);
+
+    if (!this.#events.length) {
+      render(
+        new TripMessage({ variant: MessageVariant.NoEvents }),
+        this.#headerElement,
+        RenderPosition.AFTEREND
+      );
+
+      return;
+    }
+
     render(new TripTitleView(), this.#headerElement);
     render(new TripDatesView(), this.#headerElement);
     render(new TripPriceView(), this.#headerElement);
-    render(new TripFilterView(), this.#headerElement);
-    render(new AddEventButtonView(), this.#headerElement);
     render(this.#eventListComponent, this.#headerElement, RenderPosition.AFTEREND);
     render(new TripSortView(), this.#headerElement, RenderPosition.AFTEREND);
 
