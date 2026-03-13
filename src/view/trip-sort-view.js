1adbcaa453b8c6cf68bbd64e50576cb34b01f6e7
Rustam Sadreev <rustam17.sb@gmail.com>
2026-03-13 22:45:55 +0300

    refactor(view): generate trip sort items from config

--- src/view/trip-sort-view.js
+++ src/view/trip-sort-view.js
@@ -1,45 +1,41 @@
 import { AbstractView } from '../framework';
 import { SortType } from '../constants.js';
 
+const items = [
+  { label: 'Day', value: SortType.DATE_ASC },
+  { label: 'Event' },
+  { label: 'Time', value: SortType.DURATION_DESC },
+  { label: 'Price', value: SortType.PRICE_DESC },
+  { label: 'Offers' },
+];
+
+function createTripSortItemTemplate({ label, value }, currentValue) {
+  if (value === undefined) {
+    return (
+      `<span class="trip-sort__item checker checker--emphasis">
+        <span class="checker__label">${label}</span>
+      </span>`
+    );
+  }
+
+  return (
+    `<label class="trip-sort__item checker checker--emphasis">
+      <input
+        class="checker__control visually-hidden"
+        type="radio"
+        name="type"
+        value="${value}"
+        ${value === currentValue ? 'checked' : ''}
+      >
+      <span class="checker__label">${label}</span>
+    </label>`
+  );
+}
+
 function createTripSortTemplate(currentValue) {
   return (
     `<form class="trip__sort trip-sort" action="https://echo.htmlacademy.ru/courses" method="get">
-      <label class="trip-sort__item checker checker--emphasis">
-        <input
-          class="checker__control visually-hidden"
-          type="radio"
-          name="type"
-          value="${SortType.DATE_ASC}"
-          ${SortType.DATE_ASC === currentValue ? 'checked' : ''}
-        >
-        <span class="checker__label">Day</span>
-      </label>
-      <span class="trip-sort__item checker checker--emphasis">
-        <span class="checker__label">Event</span>
-      </span>
-      <label class="trip-sort__item checker checker--emphasis">
-        <input
-          class="checker__control visually-hidden"
-          type="radio"
-          name="type"
-          value="${SortType.DURATION_DESC}"
-          ${SortType.DURATION_DESC === currentValue ? 'checked' : ''}
-        >
-        <span class="checker__label">Time</span>
-      </label>
-      <label class="trip-sort__item checker checker--emphasis">
-        <input
-          class="checker__control visually-hidden"
-          type="radio"
-          name="type"
-          value="${SortType.PRICE_DESC}"
-          ${SortType.PRICE_DESC === currentValue ? 'checked' : ''}
-        >
-        <span class="checker__label">Price</span>
-      </label>
-      <span class="trip-sort__item checker checker--emphasis">
-        <span class="checker__label">Offers</span>
-      </span>
+      ${items.map((item) => createTripSortItemTemplate(item, currentValue)).join('')}
     </form>`
   );
 }
