44a1d4796c2daa7017cfbf9e7a5a24ad9eaa5976
Rustam Sadreev <rustam17.sb@gmail.com>
2026-03-13 20:06:25 +0300

    feat: define sort types

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const SortType = {
+  DATE_ASC: 'date-asc',
+  DURATION_DESC: 'duration-desc',
+  PRICE_DESC: 'price-desc',
+};
+
+export { SortType };
--- src/view/trip-sort-view.js
+++ src/view/trip-sort-view.js
@@ -1,21 +1,22 @@
 import { AbstractView } from '../framework';
+import { SortType } from '../constants.js';
 
 function createTripSortTemplate() {
   return (
     `<form class="trip__sort trip-sort" action="https://echo.htmlacademy.ru/courses" method="get">
       <label class="trip-sort__item checker checker--emphasis">
-        <input class="checker__control visually-hidden" type="radio" name="type" value="date-asc" checked>
+        <input class="checker__control visually-hidden" type="radio" name="type" value="${SortType.DATE_ASC}" checked>
         <span class="checker__label">Day</span>
       </label>
       <span class="trip-sort__item checker checker--emphasis">
         <span class="checker__label">Event</span>
       </span>
       <label class="trip-sort__item checker checker--emphasis">
-        <input class="checker__control visually-hidden" type="radio" name="type" value="duration-desc">
+        <input class="checker__control visually-hidden" type="radio" name="type" value="${SortType.DURATION_DESC}">
         <span class="checker__label">Time</span>
       </label>
       <label class="trip-sort__item checker checker--emphasis">
-        <input class="checker__control visually-hidden" type="radio" name="type" value="price-desc">
+        <input class="checker__control visually-hidden" type="radio" name="type" value="${SortType.PRICE_DESC}">
         <span class="checker__label">Price</span>
       </label>
       <span class="trip-sort__item checker checker--emphasis">
