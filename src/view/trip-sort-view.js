ef2d801af71365bbe59f8712f65d801b57c96072
Rustam Sadreev <rustam17.sb@gmail.com>
2026-03-13 21:21:31 +0300

    feat: add sort state and change handler

--- src/view/trip-sort-view.js
+++ src/view/trip-sort-view.js
@@ -1,22 +1,38 @@
 import { AbstractView } from '../framework';
 import { SortType } from '../constants.js';
 
-function createTripSortTemplate() {
+function createTripSortTemplate(currentValue) {
   return (
     `<form class="trip__sort trip-sort" action="https://echo.htmlacademy.ru/courses" method="get">
       <label class="trip-sort__item checker checker--emphasis">
-        <input class="checker__control visually-hidden" type="radio" name="type" value="${SortType.DATE_ASC}" checked>
+        <input
+          class="checker__control visually-hidden"
+          type="radio" name="type"
+          value="${SortType.DATE_ASC}"
+          ${SortType.DATE_ASC === currentValue ? 'checked' : ''}
+        >
         <span class="checker__label">Day</span>
       </label>
       <span class="trip-sort__item checker checker--emphasis">
         <span class="checker__label">Event</span>
       </span>
       <label class="trip-sort__item checker checker--emphasis">
-        <input class="checker__control visually-hidden" type="radio" name="type" value="${SortType.DURATION_DESC}">
+        <input
+          class="checker__control visually-hidden"
+          type="radio" name="type"
+          value="${SortType.DURATION_DESC}"
+          ${SortType.DURATION_DESC === currentValue ? 'checked' : ''}
+        >
         <span class="checker__label">Time</span>
       </label>
       <label class="trip-sort__item checker checker--emphasis">
-        <input class="checker__control visually-hidden" type="radio" name="type" value="${SortType.PRICE_DESC}">
+        <input
+          class="checker__control visually-hidden"
+          type="radio"
+          name="type"
+          value="${SortType.PRICE_DESC}"
+          ${SortType.PRICE_DESC === currentValue ? 'checked' : ''}
+        >
         <span class="checker__label">Price</span>
       </label>
       <span class="trip-sort__item checker checker--emphasis">
@@ -27,7 +43,22 @@ function createTripSortTemplate() {
 }
 
 export default class TripSortView extends AbstractView {
-  _getTemplate() {
-    return createTripSortTemplate();
+  #value = null;
+  #onValueChange = null;
+
+  constructor({ value, onValueChange }) {
+    super();
+
+    this.#value = value;
+    this.#onValueChange = onValueChange;
+    this.element.addEventListener('change', this.#formChangeHandler);
   }
+
+  _getTemplate() {
+    return createTripSortTemplate(this.#value);
+  }
+
+  #formChangeHandler = (evt) => {
+    this.#onValueChange(evt.target.value);
+  };
 }
